1e39a8fc9416b69cc464b89b996aaf51ed0b78e1
Jonathan Lehner <lehnerj@student.ethz.ch>
2019-03-01 01:39:25 +0800

    added mongo express

--- indian-psychologist-app/server/main.js
+++ indian-psychologist-app/server/main.js
@@ -1,4 +1,5 @@
 import {Meteor} from "meteor/meteor";
+import { setupApi } from './imports/api'; // import our API
 
 Meteor.Mailgun.send({
     to: 'jonathanslehner@gmail.com',
@@ -6,4 +7,8 @@ Meteor.Mailgun.send({
     subject: 'A subject',
     text: 'This is the text',
     html: 'With meteor its easy to set up <strong>HTML</strong> <span style="color:red">emails</span> too.'
+});
+
+Meteor.startup(() => {  
+  setupApi(); // instantiate our new Express app
 });
